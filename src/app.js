6f1f7a0d5ec1b8ee5bf39231d5bbda3d8e25dacf
Kellen-Linse <kblinse06@gmail.com>
2022-02-22 20:06:05 -0800

    Add functionality, useContext, pagination

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 
 import React from 'react';
+import '@blueprintjs/core/lib/css/blueprint.css';
 
 import ToDo from './components/todo/todo.js';
+import SiteContextProvider from './context/SiteContext.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <ToDo />
+      <>
+        <SiteContextProvider>
+          <ToDo />
+        </SiteContextProvider>
+      </>
     );
   }
 }
